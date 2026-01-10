23f69ea9ced0aa422f7b5a2724cf8fab812bdd8d
Minh Bui <btminh.dev@gmail.com>
2026-01-10 11:07:18 +0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
 'use strict'
 
+const mongoose = require('mongoose')
 const app = require('./src/app')
 const env = require('./src/configs/env')
 const logger = require('./src/configs/logger')
+const db = require('./src/configs/database')
 
-const server = app.listen(env.PORT, () => {
-  logger.info(`Server running on port ${env.PORT}`)
-})
+let server
+
+const startServer = async () => {
+  try {
+    await db.connect()
+
+    server = app.listen(env.PORT, () => {
+      logger.info(`Server running on port ${env.PORT}`)
+    })
+  } catch (error) {
+    logger.error('Failed to start server:', error)
+    process.exit(1)
+  }
+}
+
+const exitHandler = async () => {
+  try {
+    if (server) {
+      await new Promise((resolve) => server.close(resolve))
+      logger.info('Server Express closed')
+    }
+
+    await mongoose.connection.close(false)
+    logger.info('Mongoose connection closed')
+
+    process.exit(0)
+  } catch (err) {
+    logger.error('Error closing server:', err)
+    process.exit(1)
+  }
+}
 
 process.on('SIGINT', () => {
-  server.close(() => logger.info('Server closed'))
+  console.log('System interrupt signal detected. Exiting...')
+  exitHandler()
 })
+
+process.on('SIGTERM', () => {
+  console.log('System termination signal detected. Exiting...')
+  exitHandler()
+})
+
+process.once('SIGUSR2', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through nodemon restart')
+    process.kill(process.pid, 'SIGUSR2')
+  })
+})
+
+startServer()
--- src/configs/database.js
+++ src/configs/database.js
@@ -1,4 +1,38 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const env = require('./src/configs/env')
+const env = require('./env')
+
+class Database {
+  constructor() {
+    this.connect()
+  }
+
+  async connect(type = 'mongodb') {
+    if (env.NODE_ENV === 'development') {
+      mongoose.set('debug', true)
+      mongoose.set('debug', { color: true })
+    }
+
+    try {
+      await mongoose.connect(env.MONGODB_URI, {
+        maxPoolSize: 50,
+        serverSelectionTimeoutMS: 5000,
+      })
+
+      console.log(`Connected Mongodb Success! State:`, mongoose.connection.readyState)
+    } catch (err) {
+      console.error(`Error Connect to MongoDB: ${err.message}`)
+    }
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database()
+    }
+    return Database.instance
+  }
+}
+
+const instanceMongodb = Database.getInstance()
+module.exports = instanceMongodb
